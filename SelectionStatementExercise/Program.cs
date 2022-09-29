9815140634bde857ed4604837f0c833656fb41f7
Joshua Miller <joshua.ryan.miller@icloud.com>
2022-09-28 22:08:47 -0700

    initial commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int favNumber = 7;
+            int userGuess = 0;
+            int closeAbove = favNumber + 1;
+            int closeBelow = favNumber - 1;
+
+            Console.WriteLine("Guess my favorite number between 1 & 10.");
+            do
+            {
+                userGuess = int.Parse(Console.ReadLine());
+                if (userGuess == favNumber)
+                {
+                    Console.WriteLine("Great guess!");
+                }
+                else if (userGuess == closeAbove)
+                {
+                    Console.WriteLine("Close!");
+                }
+                else if (userGuess == closeBelow)
+                {
+                    Console.WriteLine("Close!");
+                }
+                else if (userGuess < favNumber)
+                {
+                    Console.WriteLine("Too low.");
+                }
+                else if (userGuess > favNumber)
+                {
+                    Console.WriteLine("Too high.");
+                }
+                else
+                {
+                    Console.WriteLine("Nevermind.");
+                }
+            } while (userGuess != favNumber);
         }
     }
 }
